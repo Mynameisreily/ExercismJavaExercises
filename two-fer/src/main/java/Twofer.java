0786beb7427788c5ec7ee9d20b2ea710248e99fd
Reily Petty <reipet14@protonmail.com>
2019-09-16 17:34:40 -0400

    Further revision to two-fer

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,6 +1,5 @@
 class Twofer {
     String twofer(String name) {
-      String fer = (name == null) ? "One for you, one for me." : "One for " + name + ", one for me.";
-      return fer;      
+      return "One for " + (name == null ? "you" : name) + ", one for me.";      
     }
 }
