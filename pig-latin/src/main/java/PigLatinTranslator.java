b75db7d8c6317bfaf825a81d484fed1b0f07cbaa
Reily Petty <rpetty@appirio.com>
2020-04-27 18:09:17 -0400

    Submitted version of file, passed all gradle tests

--- pig-latin/src/main/java/PigLatinTranslator.java
+++ pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,25 +1,106 @@
-class PigLatinTranslator {
+import java.util.*;
 
-  import java.util.*;
+public class PigLatinTranslator {
 
   public String translate(String phrase) {
 
-    //Splitting the string for each word (if there are multiple)
-    String[] piggy = phrase.split();
+    // Splitting the string for each word (if there are multiple)
+    String[] piggy = phrase.split("\\s+");
+    String fin = "";
 
-    //Different arrays of specific things to look for in the strings
-    String[] vowel = {"xr", "yt", "a", "e", "i", "o", "u"};
-    String[] consSound = new String[""]
+    // Different arrays of specific things to look for in the strings
+    ArrayList<Character> vowel = new ArrayList<Character>();
+    vowel.add('a');
+    vowel.add('e');
+    vowel.add('i');
+    vowel.add('o');
+    vowel.add('u');
 
+    /*
+     *
+     * Rule 1 - Done
+     *
+     * Rule 2 - Done
+     *
+     * Rule 3 - Done
+     *
+     * Rule 4 - Done
+     *
+     */
 
-    //NOTE
-    //Vowel Sound  -> "-ay" at the end
-    //Consonant Qu ->
-    //Consonant y  ->
-    //Consonant    ->
+    for (String word : piggy) {
 
-    for(int i = 0; i < vowel.length(); i++) {
-      if(phrase.contains())
+      Boolean weDone = false;
+      Boolean consFlag = false;
+
+      String flipflop = "";
+      String ay = "";
+
+      word = word.toLowerCase();
+      int len = word.length();
+
+      if (len == 2 && word.charAt(1) == 'y') {
+
+        flipflop = "y" + word.charAt(0) + "ay";
+
+      } else {
+        for (int i = 0; i < len; i++) {
+
+          char letter = word.charAt(i);
+
+          if (weDone) {
+
+            flipflop += letter;
+
+          } else {
+            if (letter == 'x' || letter == 'y') {
+              if (i == 0) {
+                if ((letter == 'x') && (word.charAt(1) == 'r')) {
+
+                  flipflop += 'x';
+                  ay = "ay";
+                  weDone = true;
+
+                } else if ((letter == 'y') && (word.charAt(1) == 't')) {
+
+                  flipflop += 'y';
+                  ay = "ay";
+                  weDone = true;
+
+                } else {
+                  ay += letter;
+                  consFlag = true;
+                }
+              } else {
+                if (letter == 'y') {
+                  ay += "ay";
+                  flipflop += letter;
+                  weDone = true;
+                } else {
+                  consFlag = true;
+                  ay += letter;
+                }
+              }
+            } else if (vowel.contains(letter)) {
+              if (consFlag && (word.charAt(i - 1) == 'q') && word.charAt(i) == 'u') {
+                ay += "uay";
+                weDone = true;
+              } else {
+                ay += "ay";
+                flipflop += letter;
+                weDone = true;
+              }
+
+            } else {
+              consFlag = true;
+              ay += letter;
+            }
+          }
+        }
+        flipflop += ay;
+      }
+      fin += flipflop + " ";
     }
+    return fin.substring(0, fin.length() - 1);
   }
 }
